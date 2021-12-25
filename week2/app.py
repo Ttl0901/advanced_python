faa8443c11dc990ce70b6d0c34701c4d31bad1be
Thang Nguyen <thangdnsf@Thangs-MacBook-Pro.local>
2021-12-25 09:25:50 +0100

    update sthg

--- week2/app.py
+++ week2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +13,19 @@ def tlu():
 def author():
    return render_template('author.html')
 
+@app.route('/add')
+def add():
+    a = request.args.get('a', default = 0, type = int)
+    b = request.args.get('b', default = 0, type = int)
+    return f"{a} + {b} = {a+b}"
+
+@app.route('/mul/<int:a>/<int:b>')
+def mul(a,b):
+    return f"{a} * {b} = {a*b}"
+
+@app.route('/thread/<string:posttitle>')
+def getpage(posttitle):
+    return f"querying post via {posttitle}"
 ### REQUIREMNTS:
 """
 - create a routing which allow user to show author info: /author
